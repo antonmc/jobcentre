a6f47aea35708dccedd1860dac66001fd773b0c2
antonm <antonm@ca.ibm.com>
2016-09-15 17:33:40 -0400

    validation - segue to beacon view

--- phone/jobcentre/ViewController.swift
+++ phone/jobcentre/ViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var username : UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    @IBAction func login(sender: UIButton) {
+        // do something
+        
+        print("clicked the button");
+        
+        print(username.text);
+        print(password.text);
+        
+      // let target = "http://127.0.0.1:6025/login";
+        
+        let target = "http://cloudco.mybluemix.net/login"
+        
+        let url:URL = URL(string: target)!
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+
+        
+        let paramString = "email=" + username.text! + "&password=" + password.text!;
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest) {
+            
+            (data, response, error) in
+            
+            guard let data = data, let _:URLResponse = response  , error == nil else {
+                print("error")
+                return
+            }
+            
+            let dataString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+            
+            
+            
+            
+            
+        }
+        
+        task.resume()
+        
+        self.performSegue(withIdentifier: "beaconsegue", sender: nil)
+
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func postLogin(){
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
