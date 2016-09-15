732667a3c3c1d9e1d3c43b6e9c57abd0361dfc27
antonm <antonm@ca.ibm.com>
2016-09-15 15:34:58 -0400

    progress on #6

--- app.js
+++ app.js
@@ -7,22 +7,125 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var app = express();
+var port = process.env.PORT || 5014;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var request = require('request');
+var watson = require('watson-developer-cloud');
+
+var configDB = require('./config/database.js');
+require('./config/passport')(passport);
+
+var Account = require('./models/account');
+
+mongoose.connect(configDB.url); // connect to our database
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/isLoggedIn', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var result = {
+        outcome: 'failure'
+    };
+
+    if (req.isAuthenticated()) {
+        result.outcome = 'success';
+        result.username = req.user.local.email;
+        result.firstName = req.user.local.firstName;
+        result.lastName = req.user.local.lastName;
+    }
+
+    res.send(JSON.stringify(result, null, 3));
+})
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'html');
+
+// required for passport
+app.use(session({
+    secret: 'job-centre',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+// process the login form
+app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/loginSuccess', // redirect to the secure profile section
+    failureRedirect: '/loginFailure', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}));
+
+app.get('/loginSuccess', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        username: req.user.local.email,
+        firstName: req.user.local.first_name,
+        lastName: req.user.local.last_name,
+        outcome: 'success'
+    }, null, 3));
+})
+
+app.get('/loginFailure', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        outcome: 'failure'
+    }, null, 3));
+})
+
+app.get('/signupSuccess', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        username: req.user.local.email,
+        firstName: req.user.local.first_name,
+        lastName: req.user.local.last_name,
+        outcome: 'success'
+    }, null, 3));
+})
+
+app.get('/signupFailure', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        outcome: 'failure'
+    }, null, 3));
+})
+
+app.get('/signup', function (req, res) {
+    res.sendfile('./public/signup.html');
+});
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/signupSuccess', // redirect to the secure profile section
+    failureRedirect: '/signupFailure', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}));
+
+
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.listen(appEnv.port, '0.0.0.0', function () {
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
---
+++ public/src/jobcentre.js
@@ -0,0 +1,433 @@
+/*eslint-env browser */
+
+// Global variables to be called by Ana
+var userPolicy;
+var policyTypes;
+var policyProcedures;
+
+function openTravel() {
+    window.location = "travel.html";
+}
+
+function openTravelPolicies() {
+    window.location = "watson.html";
+}
+
+function openHealth() {
+    console.log('open health');
+}
+
+function makeAccount() {
+    console.log('makeAccount');
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    var messagearea = document.getElementById('messagearea');
+    messagearea.innerHTML = '';
+
+    console.log('email:' + email);
+
+    var xhr = new XMLHttpRequest();
+
+    var uri = 'signup';
+
+    xhr.open('POST', encodeURI(uri));
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function (response) {
+
+        var reply = JSON.parse(xhr.responseText);
+        if (xhr.status === 200) {
+            if (reply.outcome === 'success') {
+                window.location = './login'
+            } else {
+                email = '';
+                password = '';
+                messagearea.innerHTML = 'Something went wrong - try again';
+            }
+        } else if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(encodeURI('email=' + email + '&password=' + password));
+}
+
+function login() {
+    console.log('makeAccount');
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    console.log('email:' + email);
+
+    var xhr = new XMLHttpRequest();
+
+    var uri = 'login';
+
+    var messagearea = document.getElementById('messagearea');
+    messagearea.innerHTML = '';
+
+    xhr.open('POST', encodeURI(uri));
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function (response) {
+        if (xhr.status === 200) {
+
+            console.log('response');
+            console.log(xhr.responseText);
+
+            var reply = JSON.parse(xhr.responseText);
+
+            console.log(reply);
+
+            if (reply.outcome === 'success') {
+                window.location = './profile'
+            } else {
+                messagearea.innerHTML = 'Something went wrong - try again';
+            }
+
+
+        } else if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(encodeURI('email=' + email + '&password=' + password));
+}
+
+
+function get(path, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(JSON.parse(xmlhttp.responseText));
+        }
+    }
+    xmlhttp.open("GET", path, true);
+    xmlhttp.send();
+}
+
+
+function makeHistoryRow(claim) {
+
+    var date = moment(claim.date).format('YYYY-MM-DD');
+
+    var row = document.createElement('div');
+    row.className = 'claimrow';
+    row.innerHTML = '<div class="marker">' +
+        '<img class="claimimage" src="images/health/' + claim.icon + '.svg">' +
+        '</div>' +
+        '<div class="claimdata">' + claim.policy + '</div>' +
+        '<div class="claimdata">' + claim.provider + '</div>' +
+        '<div class="centereddata">' + date + '</div>' +
+        '<div class="centereddata">' + claim.outcome + '</div>' +
+        '<div class="financeclaimdata">$' + claim.amount + '</div>' +
+        '<div class="financeclaimdata">$' + claim.payment + '</div>';
+
+    return row;
+}
+
+function getClaims() {
+
+    checkStatus();
+
+    get('./history', function (reply) {
+        console.log(reply);
+
+        var header = document.getElementById('owner');
+        owner.innerHTML = reply.owner;
+
+        var claimlist = document.getElementById('claimlist');
+
+        reply.claims.forEach(function (claim) {
+            var row = makeHistoryRow(claim);
+            claimlist.appendChild(row);
+        });
+    })
+}
+
+
+function createBenefitRow(policy) {
+    var row = document.createElement('div');
+    row.className = 'benefitrow';
+    row.innerHTML = '<div class="benefiticon">' +
+        '<img class="benefitimage" src="images/health/' + policy.icon + '.svg">' +
+        '</div>' +
+        '<div class="benefitchannel">' +
+        '<div class="benefitmarker"></div>' +
+        '</div>' +
+        '<div class="benefitTitle">' + policy.title + '</div>';
+    row.onclick = function () {
+        toggleDetails(policy.title);
+    }
+
+    return row;
+}
+
+function createBenefitEntity(type) {
+
+    var benefit = document.createElement('div');
+    benefit.className = 'benefit';
+
+    benefit.innerHTML = '<div class="sideline">' + type + '</div>' +
+        '<div class="benefitblock">' +
+        '<div class="benefitcap">' +
+        '<div class="benefiticon"></div>' +
+        '<div class="benefitchanneltop"></div>' +
+        '<div class="benefitTitle"></div>' +
+        '</div>' +
+        '<div id="' + type + '" class="benefitrows">' +
+        '</div>' +
+
+        '<div class="benefitcap">' +
+        '<div class="benefiticon"></div>' +
+        '<div class="benefitchannelbottom"></div>' +
+        '<div class="benefitTitle"></div>' +
+        '</div>' +
+        '</div>';
+
+    return benefit;
+}
+
+function createBenefitDetail(policy) {
+
+    var detail = document.createElement('div');
+    detail.className = 'benefitdetail';
+    detail.id = policy.title;
+
+    detail.innerHTML = ' <div class="benefiticon">' +
+        '<div class="padding"></div>' +
+        '</div>' +
+        '<div class="benefitdetailchannel">' + '</div>' +
+        '<div class="benefitfacts">' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">benefit</div>' +
+        '<div class="factcheck">' + policy.description + '</div>' +
+        '</div>' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">limit</div>' +
+        '<div class="factcheck">$' + policy.claimLimit + '</div>' +
+        '</div>' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">coverage</div>' +
+        '<div class="factcheck">' + policy.percentCovered + '%</div>' +
+        '</div>' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">term</div>' +
+        '<div class="factcheck">' + policy.scope + '</div>' +
+        '</div>' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">start</div>' +
+        '<div class="factcheck">Jan 1 2016</div>' +
+        '</div>' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">end</div>' +
+        '<div class="factcheck">Dec 31 2017</div>' +
+        '</div>' +
+        '<div class="benefitfact">' +
+        '<div class="factlabel">code</div>' +
+        '<div class="factcheck">' + policy.code + '</div>' +
+        '</div>' +
+        '</div>'
+
+    return detail;
+}
+
+
+function toggleDetails(id) {
+    var details = document.getElementById(id);
+
+    if (details.style.display !== 'flex') {
+        details.style.display = 'flex';
+    } else {
+        details.style.display = 'none'
+    }
+
+}
+
+function unique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+
+function getBenefits() {
+
+    checkStatus();
+
+    get('./healthBenefits', function (reply) {
+        userPolicy = reply;
+
+        var header = document.getElementById('owner');
+        owner.innerHTML = reply.owner;
+
+        var policies = reply.policies;
+        var policyAreas = [];
+        var policyKeys = [];
+        var policyTitles = [];
+        policyProcedures = [];
+        var proc = [];
+
+        var benefitset = document.getElementById('benefitset');
+
+        policies.forEach(function (policy) {
+
+            if (policyAreas[policy.type]) {
+                policyAreas[policy.type].push(policy);
+                proc.push(policy.title);
+            } else {
+                policyAreas[policy.type] = [];
+                policyAreas[policy.type].push(policy);
+                policyKeys.push(policy.type);
+
+                var benefitEntity = createBenefitEntity(policy.type);
+                benefitset.appendChild(benefitEntity);
+
+                if (proc.length > 0) {
+                    policyProcedures.push(proc);
+
+                    proc = [];
+                }
+
+                proc.push(policy.title);
+            }
+
+            policyTitles.push(policy.title);
+
+            var anchor = document.getElementById(policy.type);
+
+            var benefitRow = createBenefitRow(policy);
+            var benefitDetail = createBenefitDetail(policy);
+
+            anchor.appendChild(benefitRow);
+            anchor.appendChild(benefitDetail);
+
+            policyTypes = policyKeys;
+        });
+
+        // Push the last array into the procedures array
+        policyProcedures.push(proc);
+
+        var uniquebenefits = policyTitles.filter(unique); // returns ['a', 1, 2, '1']
+
+        var select = document.getElementById('benefittypes');
+
+        uniquebenefits.forEach(function (benefit) {
+            var option = document.createElement('option');
+            option.value = benefit;
+            option.innerHTML = benefit;
+
+            select.appendChild(option);
+        })
+
+        var datepicker = document.getElementById('claimdate');
+
+        var today = moment().format('YYYY-MM-DD');
+        datepicker.value = today;
+    })
+}
+
+function submitClaim(source) {
+    var bot = false;
+    console.log("Source is: ", source);
+
+    if (source === watson) {
+        bot = true;
+    }
+
+    var claimFile = {
+        date: null,
+        benefit: null,
+        provider: null,
+        amount: null
+    };
+
+    if (source === watson) {
+        claimFile.date = context.claim_date;
+        claimFile.benefit = context.claim_procedure;
+        claimFile.provider = context.claim_provider;
+        claimFile.amount = context.claim_amount;
+    } else {
+        var dateElement = document.getElementById('claimdate');
+        var benefitElement = document.getElementById('benefittypes');
+        var providerElement = document.getElementById('provider');
+        var amountElement = document.getElementById('claimamount');
+
+        claimFile.date = dateElement.value;
+        claimFile.benefit = benefitElement.value;
+        claimFile.provider = providerElement.value;
+        claimFile.amount = amountElement.value;
+    }
+
+    var xhr = new XMLHttpRequest();
+
+    var uri = '/submitClaim';
+
+    var claimmessages = document.getElementById('claimmessages');
+
+    xhr.open('POST', uri, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function (response) {
+        if (xhr.status === 200 && xhr.responseText) {
+            var reply = JSON.parse(xhr.responseText);
+            console.log(bot);
+
+            if (reply.outcome === 'success') {
+                if (bot === true) {
+                    console.log('success');
+                    displayMessage("Your claim was successfully filed!", watson);
+                    context.claim_step = '';
+                } else {
+                    claimmessages.innerHTML = 'Your claim was filed.';
+                }
+            } else {
+                email = '';
+                password = '';
+                if (bot === true) {
+                    displayMessage("Oh no! Something went wrong. Please try again.", watson);
+                    context = '';
+                } else {
+                    claimmessages.innerHTML = 'Something went wrong - try again';
+                }
+            }
+        } else {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+
+    console.log("Submitting claim: ", JSON.stringify(claimFile));
+    xhr.send(JSON.stringify(claimFile));
+}
+
+function checkStatus() {
+
+    get('./isLoggedIn', function (reply) {
+
+        var login = document.getElementById('login');
+        var logout = document.getElementById('logout');
+
+        if (reply.outcome === 'success') {
+
+            if (login) {
+                login.style.display = 'none';
+            }
+            if (login) {
+                logout.style.display = 'inherit';
+            }
+        } else {
+            if (logout) {
+                logout.style.display = 'none';
+            }
+            if (login) {
+                login.style.display = 'inherit';
+            }
+        }
+    });
+}
+
+// Enter is pressed
+function newEvent(e, target) {
+    if (e.which === 13 || e.keyCode === 13) {
+
+        if (target === "login") {
+            login();
+        }
+    }
+}
+
+checkStatus();
